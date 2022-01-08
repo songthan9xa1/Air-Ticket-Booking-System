5a04018655b5601033d8765ad29aaf104c3c2af7
Minh Do Hai <76012525+songthan9xa1@users.noreply.github.com>
2022-01-08 16:27:55 +0100

    Create Flight.java

---
+++ Models/Flight.java
@@ -0,0 +1,17 @@
+package Models;
+
+public class Flight {
+	private int id_flight;
+	
+	public Flight(int id) {
+		this.id_flight = id;
+	}
+
+	public int getId_flight() {
+		return id_flight;
+	}
+
+	public void setId_flight(int id_flight) {
+		this.id_flight = id_flight;
+	}
+}
