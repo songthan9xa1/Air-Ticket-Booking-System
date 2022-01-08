55e444e597d2cf63372eaecabe77da339557026c
Minh Do Hai <76012525+songthan9xa1@users.noreply.github.com>
2022-01-08 16:27:16 +0100

    Create Admin.java

---
+++ Models/Admin.java
@@ -0,0 +1,31 @@
+Admin.package Models;
+
+public class Admin {
+	
+	private int ID;
+	private String email;
+	private String password;
+	
+	public Admin(String email) {
+		super();
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}
